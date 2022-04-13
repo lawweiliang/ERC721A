35ba54142fec9d3dc8deefd9a216537a4b117c49
liang <yeslaw90@hotmail.com>
2022-04-13 14:14:09 +0800

    Update: daydream migration

--- migrations/2_daydream_migration.js
+++ migrations/2_daydream_migration.js
@@ -3,7 +3,7 @@ const dayDreamContract = artifacts.require('DayDream');
 module.exports = async (deployer, network) => {
   const projectName = 'DayDream Test 1';
   const projectSymbol = 'DT1';
-  const initBaseURI = 'ipfs:// uri  /';
+  const initBaseURI = 'ipfs:// [uri]  /';
 
   await deployer.deploy(dayDreamContract, projectName, projectSymbol, initBaseURI);
 };
